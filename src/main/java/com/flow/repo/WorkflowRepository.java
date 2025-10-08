fe4bc42fbfa780954ad01fff5691fd3598917fa4
Bumblemoo <soummadeep.sen.gupta@gmail.com>
2025-10-08 23:30:19 +0530

    [WORK] - DB Configuration + Repository Configuration + sub-modules are no longer retarded.

--- src/main/java/com/flow/repo/WorkflowRepository.java
+++
@@ -1,8 +0,0 @@
-package com.flow.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.flow.model.Workflow;
-
-public interface WorkflowRepository extends JpaRepository<Workflow, Long>{
-
-}
