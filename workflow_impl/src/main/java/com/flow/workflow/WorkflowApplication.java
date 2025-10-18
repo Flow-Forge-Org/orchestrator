fd27230c77b93c796c2f3a0f5d5a460903adb6c0
Roshni <roshni06111@gmail.com>
2025-10-18 16:12:06 +0530

    Temporarily Finalizing Connections

--- workflow_impl/src/main/java/com/flow/workflow/WorkflowApplication.java
+++ workflow_impl/src/main/java/com/flow/workflow/WorkflowApplication.java
@@ -1,9 +1,15 @@
 package com.flow.workflow;
 
+import com.flowForge.annotation.EnableFlowDatabase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication()
+@EnableFlowDatabase(basePackage = "com.flow.workflow")
+@EntityScan("com.flow.entity")
+@EnableJpaRepositories("com.flow.repository")
 public class WorkflowApplication {
     public static void main(String[] args) {
         SpringApplication.run(WorkflowApplication.class, args);
